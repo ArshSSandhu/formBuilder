e0813086d49dfa00f50d6fe7ea5df021fb77db62
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-07-10 06:47:43 -0400

    loc-fix

--- src/js/control_plugins/locationControl.js
+++ src/js/control_plugins/locationControl.js
@@ -10,8 +10,8 @@ if (!window.fbControls) window.fbControls = new Array()
         return {
           icon: '📍',
           i18n: {
-            default: 'Patient Address',
-          },
+            default: 'Patient Address'
+          }
         }
       }
   
@@ -24,37 +24,48 @@ if (!window.fbControls) window.fbControls = new Array()
        * @return DOM Element to be injected into the form.
        */
       build() {
+        const streetInputId = `${this.config.name}-street`
+        const countryInputId = `${this.config.name}-country`
+        const stateInputId = `${this.config.name}-state`
+        const cityInputId = `${this.config.name}-city`
+        const zipcodeInputId = `${this.config.name}-zipcode`
+  
         return this.markup('div', [
-          this.markup('label', this.config.label),
+          this.markup('label', null),
           this.markup('input', null, {
             type: 'text',
-            id: this.config.name + '-street',
+            id: streetInputId,
             class: 'form-control',
-            placeholder: 'Street Address'
+            placeholder: 'Street Address',
+            value: this.config.value || ''
           }),
           this.markup('input', null, {
             type: 'text',
-            id: this.config.name + '-country',
+            id: countryInputId,
             class: 'form-control',
-            placeholder: 'Country'
+            placeholder: 'Country',
+            value: this.config.country || ''
           }),
           this.markup('input', null, {
             type: 'text',
-            id: this.config.name + '-state',
+            id: stateInputId,
             class: 'form-control',
-            placeholder: 'State'
+            placeholder: 'State',
+            value: this.config.state || ''
           }),
           this.markup('input', null, {
             type: 'text',
-            id: this.config.name + '-city',
+            id: cityInputId,
             class: 'form-control',
-            placeholder: 'City'
+            placeholder: 'City',
+            value: this.config.city || ''
           }),
           this.markup('input', null, {
             type: 'text',
-            id: this.config.name + '-zipcode',
+            id: zipcodeInputId,
             class: 'form-control',
-            placeholder: 'Zip Code'
+            placeholder: 'Zip Code',
+            value: this.config.zipcode || ''
           })
         ])
       }
@@ -63,45 +74,65 @@ if (!window.fbControls) window.fbControls = new Array()
        * Perform any additional actions after the control is rendered
        */
       onRender() {
-        const streetInput = document.getElementById(this.config.name + '-street')
-        const countryInput = document.getElementById(this.config.name + '-country')
-        const stateInput = document.getElementById(this.config.name + '-state')
-        const cityInput = document.getElementById(this.config.name + '-city')
-        const zipcodeInput = document.getElementById(this.config.name + '-zipcode')
+        const streetInput = document.getElementById(`${this.config.name}-street`)
+        const countryInput = document.getElementById(`${this.config.name}-country`)
+        const stateInput = document.getElementById(`${this.config.name}-state`)
+        const cityInput = document.getElementById(`${this.config.name}-city`)
+        const zipcodeInput = document.getElementById(`${this.config.name}-zipcode`)
   
         // Set the input values from the configuration if they exist
-        if (this.config.street) {
-          streetInput.value = this.config.street
-        }
-        if (this.config.country) {
-          countryInput.value = this.config.country
-        }
-        if (this.config.state) {
-          stateInput.value = this.config.state
-        }
-        if (this.config.city) {
-          cityInput.value = this.config.city
-        }
-        if (this.config.zipcode) {
-          zipcodeInput.value = this.config.zipcode
+        if (streetInput) {
+          // Set a default value if provided in config
+          if (this.config.value) {
+            streetInput.value = this.config.value
+          }
+  
+          // Add an event listener for input changes
+          streetInput.addEventListener('input', event => {
+            this.config.value = event.target.value
+            console.log('Patient address input changed to: ', event.target.value)
+          })
         }
   
-        // Update config values on input change
-        streetInput.addEventListener('input', event => {
-          this.config.street = event.target.value
-        })
-        countryInput.addEventListener('input', event => {
-          this.config.country = event.target.value
-        })
-        stateInput.addEventListener('input', event => {
-          this.config.state = event.target.value
-        })
-        cityInput.addEventListener('input', event => {
-          this.config.city = event.target.value
-        })
-        zipcodeInput.addEventListener('input', event => {
-          this.config.zipcode = event.target.value
-        })
+        if (countryInput) {
+          countryInput.value = this.config.country
+  
+          countryInput.addEventListener('input', event => {
+            if (this.config) {
+              this.config.country = event.target.value
+            }
+          })
+        }
+  
+        if (stateInput) {
+          stateInput.value = this.config.state
+  
+          stateInput.addEventListener('input', event => {
+            if (this.config) {
+              this.config.state = event.target.value
+            }
+          })
+        }
+  
+        if (cityInput) {
+          cityInput.value = this.config.city
+  
+          cityInput.addEventListener('input', event => {
+            if (this.config) {
+              this.config.city = event.target.value
+            }
+          })
+        }
+  
+        if (zipcodeInput) {
+          zipcodeInput.value = this.config.zipcode
+  
+          zipcodeInput.addEventListener('input', event => {
+            if (this.config) {
+              this.config.zipcode = event.target.value
+            }
+          })
+        }
       }
     }
   
